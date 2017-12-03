db1fca197abe8b53b6150297e41b6c68d179c074
Agerdiu <2858092937@qq.com>
2017-12-03 17:04:02 +0800

    Update showprocess.c

--- showprocess.c
+++ showprocess.c
@@ -2,51 +2,50 @@
 #include <linux/kernel.h> //KERN_INFO
 #include <linux/sched.h> //task_struct
 int traversal(void){
-    int num_running = 0; //running processes
-    int num_interruptible = 0; //interruptible processes
-    int num_uninterruptible = 0; //uninterruptible processes
-    int num_zombie = 0; //zombie processes
-    int num_stopped = 0; //stopped processes
-    int num_traced = 0; //traced processes
-    int num_dead = 0; //dead processes
-    int unknown = 0; //unknown
-    int num_total = 0; //total number of processes
-    int t_exit_state; //temp var to store task_struct.exit_state
-    int t_state; //temp var to store task_struct.state
+    int running = 0; //running processes
+    int interruptible = 0; //interruptible processes
+    int uninterruptible = 0; //uninterruptible processes
+    int zombie = 0; //zombie processes
+    int stopped = 0; //stopped processes
+    int traced = 0; //traced processes
+    int dead = 0; //dead processes
+    int unknown = 0; 
+    int total = 0; //total number of processes
+    int exit; 
+    int taskstate; //save the states of process
     struct task_struct *p; //pointer to task_struct
     printk("[PT] Process info:\n");
-    for(p=&init_task;(p=next_task(p))!=&init_task;){ //traversal
-        printk(KERN_INFO "[PT] Name:%s ; Pid:%d State:%ld ; ParName:%s ;\n",p->comm,p->pid,p->state,p->real_parent->comm); //print the process's info to log
-        num_total++; //total number of processes
-        t_state = p->state; //put p->state to variable t_state
-        t_exit_state = p->exit_state;
-        if(t_exit_state!=0){ //process exited
-            switch(t_exit_state){
-				case EXIT_ZOMBIE:
-                    num_zombie++;
+    for(p=&init_task; (p=next_task(p))!=&init_task;){ //traversal
+        printk("[PT] Name:%s ; Pid:%d State:%ld ; ParName:%s ;\n",p->comm,p->pid,p->state,p->real_parent->comm);
+        taskstate = p->state; //put p->state to variable t_state
+        exit = p->exitstate;
+        if(exit !=0){ //process exited
+            switch(exit){
+			case EXIT_ZOMBIE:
+                    zombie++;
                     break;
                 case EXIT_DEAD:
-                    num_dead++;
+                    dead++;
                     break;
                 default:
                     break;
             }
         }else{ //proess hasn't exited
-            switch(t_state){
+            switch(taskstate){
                 case TASK_RUNNING:
-                    num_running++;
+                    running++;
                     break;
                 case TASK_INTERRUPTIBLE:
-                    num_interruptible++;
-                    break;
+                    interruptible++;
+                    break;interruptible
                 case TASK_UNINTERRUPTIBLE:
-                    num_uninterruptible++;
+                    uninterruptible++;
                     break;
                 case TASK_STOPPED:
-                    num_stopped++;
+                    stopped++;
                     break;
                 case TASK_TRACED:
-                    num_traced++;
+                    traced++;
                     break;
                 default:
                     unknown++;
@@ -54,15 +53,15 @@ int traversal(void){
             }
         }
     }
-    printk(KERN_INFO "[PT] total tasks: %10d\n",num_total);
-    printk(KERN_INFO "[PT] TASK_RUNNING: %10d\n",num_running);
-    printk(KERN_INFO "[PT] TASK_INTERRUPTIBLE: %10d\n",num_interruptible);
-    printk(KERN_INFO "[PT] TASK_UNINTERRUPTIBLE: %10d\n",num_uninterruptible);
-    printk(KERN_INFO "[PT] TASK_TRACED: %10d\n",num_stopped);
-    printk(KERN_INFO "[PT] TASK_TRACED: %10d\n",num_stopped);
-    printk(KERN_INFO "[PT] EXIT_ZOMBIE: %10d\n",num_zombie);
-    printk(KERN_INFO "[PT] EXIT_DEAD: %10d\n",num_dead);
-    printk(KERN_INFO "[PT] UNKNOWN: %10d\n",unknown);
+    printk("[PT] total tasks: %10d\n",total);
+    printk("[PT] RUNNING: %10d\n",running);
+    printk("[PT] INTERRUPTIBLE: %10d\n",interruptible);
+    printk("[PT] UNINTERRUPTIBLE: %10d\n",uninterruptible);
+    printk("[PT] STOPPED: %10d\n",stopped);
+    printk("[PT] TRACED: %10d\n",traced);
+    printk("[PT] ZOMBIE: %10d\n",zombie);
+    printk("[PT] DEAD: %10d\n",dead);
+    printk("[PT] Unknown: %10d\n",unknown);
     return 0;
 } 
 
